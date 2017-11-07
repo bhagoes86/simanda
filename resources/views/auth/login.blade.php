8b7b772db182972fc97bcaa06a4b6787301a8f90
ranitachi <fachran.nazarullah@gmail.com>
2017-11-07 15:11:43 +0700

    update-add logo pemkot depok

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,10 +22,10 @@
         <div class="app app-fh">
 
             <!-- START APP CONTAINER -->
-            <div class="app-container" style="background: url(assets/images/background/bg-1.jpg) center center no-repeat fixed;">
+            <div class="app-container">
                 
                 <div class="app-login-box">                                        
-                    <div class="app-login-box-user"><img src="{{ asset('theme/img/user/no-image.png') }}"></div>
+                    <div class="text-center" style="margin:30px auto;"><img style="height:100px;float:none;" src="{{ asset('theme/img/logo-depok.png') }}"></div>
                     <div class="app-login-box-title">
                         <div class="title">Simanda 2017</div>
                         <div class="subtitle">Silahkan lakukan login</div>                        
@@ -61,18 +61,51 @@
         <!-- END APP WRAPPER -->                
 
         <!-- IMPORTANT SCRIPTS -->
-        <script type="text/javascript" src="js/vendor/jquery/jquery.min.js"></script>
-        <script type="text/javascript" src="js/vendor/jquery/jquery-migrate.min.js"></script>
-        <script type="text/javascript" src="js/vendor/jquery/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="js/vendor/bootstrap/bootstrap.min.js"></script>
-        <script type="text/javascript" src="js/vendor/moment/moment.min.js"></script>
-        <script type="text/javascript" src="js/vendor/customscrollbar/jquery.mCustomScrollbar.min.js"></script>
+        <script type="text/javascript" src="{{ asset('theme/js/vendor/jquery/jquery.min.js')}}"></script>
+        <script type="text/javascript" src="{{ asset('theme/js/vendor/jquery/jquery-migrate.min.js')}}"></script>
+        <script type="text/javascript" src="{{ asset('theme/js/vendor/jquery/jquery-ui.min.js')}}"></script>
+        <script type="text/javascript" src="{{ asset('theme/js/vendor/bootstrap/bootstrap.min.js')}}"></script>
+        <script type="text/javascript" src="{{ asset('theme/js/vendor/moment/moment.min.js')}}"></script>
+        <script type="text/javascript" src="{{ asset('theme/js/vendor/customscrollbar/jquery.mCustomScrollbar.min.js')}}"></script>
         <!-- END IMPORTANT SCRIPTS -->
        
         <!-- APP SCRIPTS -->
-        <script type="text/javascript" src="js/app.js"></script>
-        <script type="text/javascript" src="js/app_plugins.js"></script>
-        <script type="text/javascript" src="js/app_demo.js"></script>
+        <script type="text/javascript" src="{{ asset('theme/js/app.js')}}"></script>
+        <script type="text/javascript" src="{{ asset('theme/js/app_plugins.js')}}"></script>
         <!-- END APP SCRIPTS -->
     </body>
 </html>
+<style>
+.app-container
+{
+    float: left;
+    width: 100%;
+    position: relative;
+    background: #EDEFF0;
+}
+.app-container:before
+{
+    content: ' ';
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    opacity: 0.4;
+    background:blue url('theme/img/bg-simanda.jpg');
+    background-repeat: no-repeat;
+    background-position: 50% 0;
+    -ms-background-size: cover;
+    -o-background-size: cover;
+    -moz-background-size: cover;
+    -webkit-background-size: cover;
+    background-size: cover;
+}
+.app-login-box
+{
+    z-index: 2;
+    position: relative;
+}
+</style>
