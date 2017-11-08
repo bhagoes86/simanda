0313a08bbbd4888cacae006b7daaa9ca48a0f6eb
dudyali <dudyali@gmail.com>
2017-11-08 12:11:12 +0700

    disable logout route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,7 @@
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
-                                        <a href="{{ route('logout') }}"
+                                        {{--  <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                             Logout
@@ -66,7 +66,7 @@
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                             {{ csrf_field() }}
-                                        </form>
+                                        </form>  --}}
                                     </li>
                                 </ul>
                             </li>
